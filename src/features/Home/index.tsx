51e0709982c28c56595ab9c7ecf118f8f662a5d8
Nilanchal9437 <nilanchal.patra3777@gmail.com>
2026-03-24 13:37:05 +0530

    Mobile fixed

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -41,15 +41,15 @@ export default function Home() {
     <main className="bg-cover bg-no-repeat bg-center bg-[url('/mobile-space-bg.png')] md:bg-[url('/space-bg.png')] h-[100%]">
       {/* Rotating "fire ring" + wheel behind the form */}
       <div className="pointer-events-none rounded-full absolute left-1/2 top-[10px] z-0 -translate-x-1/2">
-        <div className="relative h-[330px] w-[330px] sm:h-[420px] sm:w-[420px] md:h-[600px] md:w-[600px]">
+        <div className="relative h-[265px] w-[265px] sm:h-[420px] sm:w-[420px] md:h-[600px] md:w-[600px]">
           {/* Inner glow */}
-          <div className="absolute top-[14vh]  inset-10 z-5 rounded-full bg-[radial-gradient(circle_at_center,rgba(245,208,108,0.62),transparent_65%)] blur-[8px] opacity-90" />
+          <div className="absolute top-[25vh] md:top-[14vh]  inset-10 z-5 rounded-full bg-[radial-gradient(circle_at_center,rgba(245,208,108,0.62),transparent_65%)] blur-[8px] opacity-90" />
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src="/zodiac-wheel.png"
             alt="Zodiac wheel"
             draggable={false}
-            className="absolute top-[14vh] inset-0 z-10 h-full w-full select-none rounded-full object-cover animate-spin-slow drop-shadow-[0_0_18px_rgba(245,208,108,0.35)] drop-shadow-[0_0_48px_rgba(245,208,108,0.18)]"
+            className="absolute top-[25vh] md:top-[14vh] inset-0 z-10 h-full w-full select-none rounded-full object-cover animate-spin-slow drop-shadow-[0_0_18px_rgba(245,208,108,0.35)] drop-shadow-[0_0_48px_rgba(245,208,108,0.18)]"
           />
         </div>
       </div>

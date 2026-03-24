c68e98bbf95d8ef317df56116231d959b8ab676a
Nilanchal9437 <nilanchal.patra3777@gmail.com>
2026-03-24 14:13:25 +0530

    Bug fxied

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -38,21 +38,7 @@ export default function Home() {
   const [year, setYear] = useState<string>("");
 
   return (
-    <main className="bg-cover bg-no-repeat bg-center bg-[url('/mobile-space-bg.png')] md:bg-[url('/space-bg.png')] h-[100%]">
-      {/* Rotating "fire ring" + wheel behind the form */}
-      <div className="pointer-events-none rounded-full absolute left-1/2 top-[10px] z-0 -translate-x-1/2">
-        <div className="relative h-[265px] w-[265px] md:h-[550px] md:w-[550px] xl:h-[600px] xl:w-[600px]">
-          {/* Inner glow */}
-          <div className="absolute top-[25vh] md:top-[14vh]  inset-10 z-5 rounded-full bg-[radial-gradient(circle_at_center,rgba(245,208,108,0.62),transparent_65%)] blur-[8px] opacity-90" />
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/zodiac-wheel.png"
-            alt="Zodiac wheel"
-            draggable={false}
-            className="absolute top-[25vh] md:top-[14vh] inset-0 z-10 h-full w-full select-none rounded-full object-cover animate-spin-slow drop-shadow-[0_0_18px_rgba(245,208,108,0.35)] drop-shadow-[0_0_48px_rgba(245,208,108,0.18)]"
-          />
-        </div>
-      </div>
+    <main className="h-[100%]">
       <div className="lg:h-[85vh] bg-[rgba(2,11,26,0.15)] shadow-[0_30px_80px_rgba(0,0,0,0.65),0_0_60px_rgba(245,208,108,0.2)]">
         <Container className="relative pt-12 bg-[rgba(2,11,26,0.15)] shadow-[0_30px_80px_rgba(0,0,0,0.65),0_0_60px_rgba(245,208,108,0.2)] md:bg-transparent md:shadow-none">
           <section className="relative overflow-hidden lg:h-[85vh]">
